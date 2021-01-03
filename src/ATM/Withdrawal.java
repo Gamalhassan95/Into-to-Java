3b1dd14523134d28bca0ac33c06c93ce546d65e8
Gamal Hassan <gh904@nyu.edu>
2021-01-03 12:55:54 -0500

    updates

---
+++ src/ATM/Withdrawal.java
@@ -0,0 +1,17 @@
+package ATM;
+
+public class Withdrawal extends transaction
+{
+    private double amount;
+    private Keypad keypad;
+    private CashDispenser cashDispenser;
+
+    public Withdrawal()
+    {
+
+    }
+    @Override
+    public void execute(){
+
+    }
+}
